89a57d7cba7f9fb7085c1e92d025bcbe5f5b4f91
Guilherme Figueira <guilhermefig@outlook.com>
2024-04-25 20:33:43 -0300

    Metodos

--- src/guilherme/operadores/Operadores.java
+++ src/guilherme/operadores/Operadores.java
@@ -4,56 +4,65 @@ package guilherme.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        //Atribuição
-        String nome = "GLEYSON";
-        int idade = 22;
-        double peso = 68.5;
-        char sexo = 'M';
-        boolean doadorOrgao = false;
-        // Date dataNascimento = new Date();
+        // //Atribuição
+        // String nome = "GLEYSON";
+        // int idade = 22;
+        // double peso = 68.5;
+        // char sexo = 'M';
+        // boolean doadorOrgao = false;
+        // // Date dataNascimento = new Date();
 
-        //Aritiméticos
-        double soma = 10.5 + 15.7;
-        int subtração = 113 - 25;
-        int multiplicacao = 20 * 7;
-        int divisao = 15 / 3;
-        int modulo = 18 % 3;
-        double resultado = (10 * 7) + (20/4);
+        // //Aritiméticos
+        // double soma = 10.5 + 15.7;
+        // int subtração = 113 - 25;
+        // int multiplicacao = 20 * 7;
+        // int divisao = 15 / 3;
+        // int modulo = 18 % 3;
+        // // double resultado = (10 * 7) + (20/4);
 
-        //Concatenação com "+"
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-            // System.out.println(nomeCompleto);
-        String concatenacao ="?"; 
+        // //Concatenação com "+"
+        // String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //     // System.out.println(nomeCompleto);
+        // String concatenacao ="?"; 
 
-        // System.out.println(concatenacao);
+        // // System.out.println(concatenacao);
 
-        // concatenacao = 1+1+1+"1";
+        // // concatenacao = 1+1+1+"1";
 
-        // System.out.println(concatenacao);
+        // // System.out.println(concatenacao);
             
-        // concatenacao = 1+"1"+1+1;
+        // // concatenacao = 1+"1"+1+1;
 
-        // System.out.println(concatenacao);
+        // // System.out.println(concatenacao);
             
-        // concatenacao = 1+"1"+1+"1";
+        // // concatenacao = 1+"1"+1+"1";
 
-        // System.out.println(concatenacao);
+        // // System.out.println(concatenacao);
             
-        // concatenacao = "1"+1+1+1;
+        // // concatenacao = "1"+1+1+1;
 
-        // System.out.println(concatenacao);
+        // // System.out.println(concatenacao);
             
-        // concatenacao = "1"+(1+1+1);
+        // // concatenacao = "1"+(1+1+1);
 
-        // System.out.println(concatenacao);
+        // // System.out.println(concatenacao);
 
 
-        //Unários
-        int numero = 5;
+        // //Unários
+        // int numero = 5;
 
-        numero = - numero;
-        System.out.println(numero);
-        numero = numero * -1 ;
-        System.out.println(numero);
+        // numero = - numero;
+        // System.out.println(numero);
+        // numero = numero * -1 ;
+        // System.out.println(numero);
+
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+        
     }
 }
