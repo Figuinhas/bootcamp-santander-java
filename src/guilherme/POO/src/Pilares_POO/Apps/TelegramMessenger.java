1d8c74faa78ab5f1e9f6914c880c5372d8badeee
Guilherme Figueira <guilhermefig@outlook.com>
2024-05-08 21:08:53 -0300

    Pilares POO

---
+++ src/guilherme/POO/src/Pilares_POO/Apps/TelegramMessenger.java
@@ -0,0 +1,10 @@
+package Apps;
+public class TelegramMessenger extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem pelo Telegram");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem pelo Telegram");
+	}
+}
---
+++ src/guilherme/POO/src/Pilares_POO/ComputadorPedrinho.java
@@ -0,0 +1,28 @@
+import Apps.FacebookMessenger;
+import Apps.MSNMessenger;
+import Apps.ServicoMensagemInstantanea;
+import Apps.TelegramMessenger;
+
+public class ComputadorPedrinho {
+	public static void main(String[] args) {
+		
+		ServicoMensagemInstantanea smi = null;
+		
+		/*
+		    NÃO SE SABE QUAL APP 
+		    MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+		 */
+		String appEscolhido="fbm"; 
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new FacebookMessenger();
+		else if(appEscolhido.equals("tlg"))
+			smi = new TelegramMessenger();
+		
+			
+		smi.enviarMensagem();
+		smi.receberMensagem();
+	}
+}
