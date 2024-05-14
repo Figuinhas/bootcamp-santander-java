7f38e23ae228ebbb681f262a612e5cc80c054824
Guilherme Figueira <guilhermefig@outlook.com>
2024-05-14 19:53:05 -0300

    Exercicio - Carrinho de compras

---
+++ src/guilherme/collections/list/CarrinhoDeCompras.java
@@ -0,0 +1,52 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CarrinhoDeCompras {
+    //atributo
+    private List<Item> carrinhoCompras;
+
+    public CarrinhoDeCompras() {
+        this.carrinhoCompras = new ArrayList<>();
+    }
+    public void adicionarItem(String nome, double preco, int quantidade){
+        carrinhoCompras.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for(Item item : carrinhoCompras){
+            if(item.getNome().equalsIgnoreCase(nome)){
+                itensParaRemover.add(item);
+            }
+        }
+        carrinhoCompras.removeAll(itensParaRemover);
+    }
+
+    public void calcularValorTotal(){
+        double valorTotal = 0;
+        for(Item item : carrinhoCompras){
+            valorTotal = valorTotal +(item.getPreco()*item.getQuantidade());
+        }
+        System.out.println(valorTotal);
+    }
+
+    public void exibirItens(){
+        System.out.println(carrinhoCompras);
+    }
+    
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+
+        carrinho.adicionarItem("Item 1",2.50, 4);
+        carrinho.adicionarItem("Item 2",2.50, 4);
+        carrinho.adicionarItem("Item 3",2.50, 4);
+        carrinho.adicionarItem("Item 4",2.50, 4);
+
+        carrinho.calcularValorTotal();
+
+        carrinho.exibirItens();
+    }
+}
